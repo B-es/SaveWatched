3cec0044af5df73c614a7fbea468526bd5a393b9
B-es <ivan2003064@gmail.com>
2023-02-07 21:10:13 +0300

    main

---
+++ main.py
@@ -0,0 +1,12 @@
+import customtkinter as ctk
+
+class App(ctk.CTk):
+    def __init__(self):
+        super().__init__()
+        
+        self.geometry("500x500")
+        self.title("SaveWatched")
+        
+if __name__ == "__main__":
+    app = App()
+    app.mainloop()
